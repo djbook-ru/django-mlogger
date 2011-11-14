36ce7bbe7ed0f46f31ca367ba7866edb8c89c7b5
Ruslan Popov <ruslan.popov@gmail.com>
2011-11-14 12:32:39 +0400

    Packaging.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='mlogger',
+      version='0.1',
+      description='Application that helps your models to keep track of all their changes.',
+      author='Ruslan Popov',
+      author_email='ruslan.popov@gmail.com',
+      url='http://github.com/RaD/django-mlogger/tree/master',
+      packages=['securelayer',],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
